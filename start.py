3bbda03d11905285e7e2b5528425cc468639e548
Slluxx <Slluxx@users.noreply.github.com>
2022-05-25 12:27:12 +0200

    Init

---
+++ start.py
@@ -0,0 +1,13 @@
+import eel
+import urllib.request
+
+eel.init('web')
+
+@eel.expose
+def print_string(text):
+    print(text)
+    
+    return True
+
+eel.fromPy_alert("Eel started")
+eel.start('index.html', size=(640, 320))
